e124f7d788d9b2d7affc28ec3d93b8eb58cc388f
esha <acharya.e@northeastern.edu>
2023-05-30 18:41:51 -0400

    added responsiveness to navigation bar

--- src/tuiter/explore-screen/index.js
+++ src/tuiter/explore-screen/index.js
@@ -8,16 +8,14 @@ function ExploreScreen() {
    <>
      <div className="row">
        <div className="col-11 position-relative">
-         <input placeholder="Search Tuiter"
-                className="form-control rounded-pill ps-5"/>
-         <AiOutlineSearch className="fs-3 position-absolute 
-                       wd-nudge-up"/>
+         <input placeholder="Search Tuiter" className="form-control rounded-pill ps-5"/>
+         <AiOutlineSearch className="fs-3 position-absolute wd-nudge-up"/>
        </div>
        <div className="col-1">
-         <GoGear className="wd-top-4 float-end
-                       fs-3 position-relative"/>
+         <GoGear className="wd-top-4 float-end fs-3 position-relative"/>
        </div>
      </div>
+
      <ul className="nav nav-pills mb-2 mt-2">
        <li className="nav-item">
          <a className="nav-link active">For You</a>
@@ -35,11 +33,12 @@ function ExploreScreen() {
          <a className="nav-link">Entertainment</a>
        </li>
      </ul>
+
      <div className="position-relative mb-2">
        <img src="/images/starship1.jpg" className="w-100"/>
-       <h1 className="position-absolute wd-nudge-up text-white">
-         SpaceX Starship</h1>
+       <h1 className="position-absolute wd-nudge-up text-white">SpaceX Starship</h1>
      </div>
+
      <TuitSummaryList/>
    </>
  );
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FaTwitter, FaHome, FaSearch, FaBell, FaEnvelope, FaBookmark, FaList, FaUser, FaEllipsisH,FaHashtag} from 'react-icons/fa';
+
 const NavigationSidebar = () => {
  const { pathname } = useLocation();
  const [ignore, tuiter, active] = pathname.split("/");
- const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
+ const links = [
+  { name: "home", icon: <FaHome />, text: "Home" },
+  { name: "explore", icon: <FaHashtag />, text: "Explore" },
+  { name: "notifications", icon: <FaBell />, text: "Notifications" },
+  { name: "messages", icon: <FaEnvelope />, text: "Messages" },
+  { name: "bookmarks", icon: <FaBookmark />, text: "Bookmarks" },
+  { name: "lists", icon: <FaList />, text: "Lists" },
+  { name: "profile", icon: <FaUser />, text: "Profile" },
+  { name: "more", icon: <FaEllipsisH />, text: "More" },
+];
+
  return (
    <div className="list-group">
-     {links.map((link) => 
-         <Link to={`/tuiter/${link}`} className={`list-group-item text-capitalize ${active === link ? "active" : ""}`}>
-           {link}
-         </Link>
-     )}
+    <div class="fa-1x text-black list-group-item list-group-item-action"><FaTwitter /></div>
+     {links.map((link) => (
+          <Link
+          key={link.name}
+          to={`/tuiter/${link.name}`}
+          className={`list-group-item d-flex align-items-center text-capitalize ${
+            active === link.name ? "active" : ""
+          }`}
+        >
+          <div className="me-2">{link.icon}</div>
+          <span className="d-none d-xl-inline d-xxl-inline text-wrap">{link.text}</span>
+        </Link>
+      ))}
    </div>
  );
 };
