8f056430199f94536adea7c2dd416575febbb405
esha <acharya.e@northeastern.edu>
2023-06-13 07:38:26 -0400

    a5 fixed another bug

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-//const TUITS_API = 'http://localhost:4000/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const TUITS_API = `${SERVER_API_URL}/tuits`
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit)
